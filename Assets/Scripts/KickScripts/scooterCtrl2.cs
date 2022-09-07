debfa972a09ea79df2451827390061dc8905a73e
hnh3883@naver.com <>
2022-09-07 12:41:02 +0900

    파워핸들 제거

--- Assets/Scripts/KickScripts/scooterCtrl2.cs
+++ Assets/Scripts/KickScripts/scooterCtrl2.cs
@@ -6,7 +6,9 @@ public class scooterCtrl2 : MonoBehaviour
     public bool activeSelf;
 
     public Transform handleBar;
-    public WheelCollider colliderF;
+    public WheelCollider colliderF1;
+    public WheelCollider colliderF2;
+
 
     public float highestSpeed = 500f;
 	public float lowSpeedSteerAngle = 0.1f;
@@ -14,9 +16,12 @@ public class scooterCtrl2 : MonoBehaviour
 
     private float prevSteerAngle;
 
+    float GetGetAxis;
+
     private void Awake() 
     {
-        StartCanvas = GameObject.Find("StartCanvas");   
+        StartCanvas = GameObject.Find("StartCanvas");
+        GetGetAxis = 0;
     }
 
     void FixedUpdate()
@@ -34,17 +39,38 @@ public class scooterCtrl2 : MonoBehaviour
         float steerAngle = Mathf.Lerp(lowSpeedSteerAngle, highSpeedStreerAngle, 1 / speedFactor);
         //print ("steerAngle:" + steerAngle);
         // x*=y-> x = x*y
-        steerAngle *= Input.GetAxis("Horizontal") / 2;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (GetGetAxis >= -0.5)
+            {
+                GetGetAxis = GetGetAxis - 0.03f;
+            }
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (GetGetAxis <= 0.5)
+            {
+                GetGetAxis = GetGetAxis + 0.03f;
+            }
+        }
+
+
+        steerAngle *= GetGetAxis;
+
+        //steerAngle *= Input.GetAxis("Horizontal") / 2;
+        
 
         //좌우 방향전환
-        colliderF.steerAngle = steerAngle;
+        colliderF1.steerAngle = steerAngle;
+        colliderF2.steerAngle = steerAngle;
     }
 
     void Update() 
     {
 		// 앞바퀴 2개를 이동방향으로 향하기
-		handleBar.Rotate (Vector3.up, colliderF.steerAngle-prevSteerAngle, Space.World);
-		prevSteerAngle = colliderF.steerAngle;
+		handleBar.Rotate (Vector3.up, colliderF1.steerAngle-prevSteerAngle, Space.World);
+		prevSteerAngle = colliderF1.steerAngle;
 	}
 
 
